6d60e3936957597c61b19a067336ba4a7ba3236c
Michael <michael.beenen@gmail.com>
2014-08-23 06:54:42 -0700

    Very simpleton date filter for statistics

--- routes/stats.js
+++ routes/stats.js
@@ -26,80 +26,85 @@ exports.view = function(req, res, next) {
       if (!deck) {
         return next(new Error("You chose a broken deck"));
       }
-      req.models.Game.find({deck: deck._id})
-        .populate('deck')
-        .exec(function (error, games) {
+      var gameQuery = req.models.Game.find({deck: deck._id})
+            .populate('deck');
+      if (req.query.days) {
+        var queryDate = new Date();
+        queryDate.setDate(queryDate.getDate() + (-1 * req.query.days));
+        gameQuery.where('date').gt(queryDate);
+      }
+      gameQuery.exec(function (error, games) {
+        if (error) {
+          return next(error);
+        }
+        console.log('games is ' + util.inspect(games));
+        req.models.Archetype.list(function(error, archetypes) {
           if (error) {
             return next(error);
           }
-          console.log('games is ' + util.inspect(games));
-          req.models.Archetype.list(function(error, archetypes) {
-            if (error) {
-              return next(error);
+          console.log('archetypes is ' + util.inspect(archetypes));
+          var statsMap = {
+            All: {
+              name: 'All',
+              deckStats: {}
             }
-            console.log('archetypes is ' + util.inspect(archetypes));
-            var statsMap = {
-              All: {
-                name: 'All',
-                deckStats: {}
-              }
+          };
+          statsMap['All']['deckStats'][deck._id] = {
+            name: deck.name,
+            games: 0,
+            wins: 0,
+            losses: 0,
+            draws: 0,
+            winRate: "N/A"
+          };
+          archetypes.forEach(function(archetype, index) {
+            var archetypeKey = archetype.class + '-' + archetype.name;
+            statsMap[archetype._id] = {
+              name: archetypeKey,
+              deckStats: {}
             };
-            statsMap['All']['deckStats'][deck._id] = {
-                    name: deck.name,
-                    games: 0,
-                    wins: 0,
-                    losses: 0,
-                    draws: 0,
-                    winRate: "N/A"
+            statsMap[archetype._id]['deckStats'][deck._id] = {
+              name: deck.name,
+              games: 0,
+              wins: 0,
+              losses: 0,
+              draws: 0,
+              winRate: "N/A"
             };
-            archetypes.forEach(function(archetype, index) {
-              var archetypeKey = archetype.class + '-' + archetype.name;
-              statsMap[archetype._id] = {
-                name: archetypeKey,
-                deckStats: {}
-              };
-              statsMap[archetype._id]['deckStats'][deck._id] = {
-                name: deck.name,
-                games: 0,
-                wins: 0,
-                losses: 0,
-                draws: 0,
-                winRate: "N/A"
-              };
-            });
-            games.forEach(function(game) {
-              var gameDeck = game.deck._id;
-              var opponentArchetype = game.opponentArchetype;
-              var archetypeDeckStats = statsMap[opponentArchetype]['deckStats'][gameDeck];
-              var totalDeckStats = statsMap['All']['deckStats'][gameDeck];
-              totalDeckStats.games++;
-              archetypeDeckStats.games++;
-              switch(game.result) {
-              case 0:
-                totalDeckStats.losses++;
-                archetypeDeckStats.losses++;
-                break;
-              case 1:
-                totalDeckStats.draws++;
-                archetypeDeckStats.draws++;
-                break;
-              case 2:
-                totalDeckStats.wins++;
-                archetypeDeckStats.wins++;
-                break;
-              default:
-                break;
-              }
-              archetypeDeckStats.winRate = numeral((archetypeDeckStats.wins) / (archetypeDeckStats.games - archetypeDeckStats.draws)).format('0.00');
-              totalDeckStats.winRate = numeral((totalDeckStats.wins) / (totalDeckStats.games - totalDeckStats.draws)).format('0.00');
-            });
-            console.log('stats map is ' + util.inspect(statsMap, false, 5));
-            res.render('stats', {
-              decks: decks,
-              statsMap: statsMap
-            });
+          });
+          games.forEach(function(game) {
+            var gameDeck = game.deck._id;
+            var opponentArchetype = game.opponentArchetype;
+            var archetypeDeckStats = statsMap[opponentArchetype]['deckStats'][gameDeck];
+            var totalDeckStats = statsMap['All']['deckStats'][gameDeck];
+            totalDeckStats.games++;
+            archetypeDeckStats.games++;
+            switch(game.result) {
+            case 0:
+              totalDeckStats.losses++;
+              archetypeDeckStats.losses++;
+              break;
+            case 1:
+              totalDeckStats.draws++;
+              archetypeDeckStats.draws++;
+              break;
+            case 2:
+              totalDeckStats.wins++;
+              archetypeDeckStats.wins++;
+              break;
+            default:
+              break;
+            }
+            archetypeDeckStats.winRate = numeral((archetypeDeckStats.wins) / (archetypeDeckStats.games - archetypeDeckStats.draws)).format('0.00');
+            totalDeckStats.winRate = numeral((totalDeckStats.wins) / (totalDeckStats.games - totalDeckStats.draws)).format('0.00');
+          });
+          console.log('stats map is ' + util.inspect(statsMap, false, 5));
+          res.render('stats', {
+            decks: decks,
+            statsMap: statsMap
           });
         });
+      });
     });
   });
 };
